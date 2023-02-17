a96c8f1d9c15aaaf92b260e46e19884fc085bc80
abdelwahedmodest <abdelwahedmodest@gmail.com>
2023-02-17 06:35:14 +0100

    three  files are  added

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, render_template
+import subprocess
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def generate_html():
+    if request.method == 'POST':
+        desktop_path = request.form['desktop_path']
+        subprocess.call(['bash', 'genhtml5_1.sh', desktop_path])
+        return 'HTML table generated!'
+    else:
+        return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
